27fda42ee227b2a0ab54d47869b7ef713afb2a4c
sniedzespele <42698962+sniedzespele@users.noreply.github.com>
2019-02-12 14:16:34 +0200

    Create lower.py

---
+++ python/lower.py
@@ -0,0 +1,8 @@
+
+jautājums = input(" Vai izpildijāt mājasdarbus? Jā/Nē: ")
+while True:
+    if question.lower() == "jā" or question.lower() == "j":
+        print("Labi darīts!")
+    elif question.lower() == "nē" or question.lower() == "n":
+        print("Žēl, vajadzēja pacensties! :)")
+    break
